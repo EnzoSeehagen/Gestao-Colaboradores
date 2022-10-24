391996ff78c31366c5c43dd70d7bc5f8be27a745
EnzoSeehagen <enzovonseehagen@gmail.com>
2022-10-24 20:55:33 -0300

    Aplicando controle de estado

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Botao from "../Botao";
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
@@ -16,21 +17,34 @@ const Formulario = () => {
 
   ]
 
+  const [nome, setNome] = useState('')
+  const [casa, setCasa] = useState('')
+  const [imagem, setImagem] = useState('')
+
+
   const aoSalvar = (evento) => {
     evento.preventDefault()
-    console.log('Form enviado!')
+    console.log('Form enviado!', nome, casa, imagem)
   }
 
   return (
     <section className="formulario">
       <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do personagem</h2>
-        <CampoTexto obrigatorio={true} label="" placeholder="Digite o nome do personagem" />
-        <CampoTexto obrigatorio={true} label="" placeholder="Digite a sua casa" />
-        <CampoTexto obrigatorio={true} label="" placeholder="Digite o endereço da imagem " />
-        <ListaSuspensa obrigatorio={true} label="" placeholder="Digite o tipo do personagem " itens={times}/>
+        <CampoTexto 
+              valor={nome} obrigatorio={true} label="" 
+              placeholder="Digite o nome do personagem"  aoAlterado={valor => setNome(valor)}/>
+        <CampoTexto 
+              valor={casa} obrigatorio={true} label="" 
+              placeholder="Digite a sua casa" aoAlterado={valor => setCasa(valor)}/>
+        <CampoTexto 
+              valor={imagem} obrigatorio={true} label="" 
+              placeholder="Digite o endereço da imagem " aoAlterado={valor => setImagem(valor)} />
+        <ListaSuspensa 
+              obrigatorio={true} label="" 
+              placeholder="Digite o tipo do personagem " itens={times}/>
         <Botao>
-          Savar e cadastrar Card do personagem
+              Savar e cadastrar Card do personagem
         </Botao> 
       </form>
     </section>
